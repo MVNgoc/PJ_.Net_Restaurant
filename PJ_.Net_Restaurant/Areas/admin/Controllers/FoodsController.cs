70d8af8fc2c4c205b16c96e16f6e8b82fe60e90c
Mai Ngọc <mvn.bp01@gmail.com>
2023-05-11 13:31:04 +0700

    create food manager

---
+++ PJ_.Net_Restaurant/Areas/admin/Controllers/FoodsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PJ_.Net_Restaurant.Models;
+
+namespace PJ_.Net_Restaurant.Areas.admin.Controllers
+{
+    public class FoodsController : Controller
+    {
+        private RestaurantEntities db = new RestaurantEntities();
+
+        // GET: admin/Foods
+        public ActionResult Index()
+        {
+            return View(db.Foods.ToList());
+        }
+
+        // GET: admin/Foods/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food);
+        }
+
+        // GET: admin/Foods/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: admin/Foods/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,img,price,discountprice,describe,sales,type,footstyle,link,meta,hide,order,datebegin")] Food food)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Foods.Add(food);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(food);
+        }
+
+        // GET: admin/Foods/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food);
+        }
+
+        // POST: admin/Foods/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,img,price,discountprice,describe,sales,type,footstyle,link,meta,hide,order,datebegin")] Food food)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(food).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(food);
+        }
+
+        // GET: admin/Foods/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food);
+        }
+
+        // POST: admin/Foods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Food food = db.Foods.Find(id);
+            db.Foods.Remove(food);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
